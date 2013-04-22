4e5bff65b5ebb7a33158d07629f2a2ba75f36e22
Spencer <spencersevilla@gmail.com>
2013-04-22 16:26:45 -0700

    adding first attempt at using schedule_delayed_work...

--- src/module_hooks.c
+++ src/module_hooks.c
@@ -1,17 +1,27 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/workqueue.h>
+#include <linux/sched.h> 
 
 int timerCount = 0;
 
+static struct work_struct task;
+static DECLARE_WORK(task, intrpt_routine, NULL);
+
 /*******************************************************************************/
-int init_module(void)
-{
+int init_module(void) {
     printk(KERN_INFO "adding add module!");
+    schedule_delayed_work(&task, 5*HZ);
     return 0;
 }
 
-void cleanup_module(void)
-{
+static void intrpt_routine(void *irrelevant) {
+    printk(KERN_INFO "INTERRUPT!!!");
+	schedule_delayed_work(&task, 5*HZ);
+}
+
+
+void cleanup_module(void) {
     printk(KERN_INFO "removing add module!");
 }
 
