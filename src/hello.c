afcb39a96dd8bac36b1d8d0497cabef400132a1f
Spencer <spencersevilla@gmail.com>
2013-04-26 18:06:06 -0700

    cleaning up code a bit
    starting to map-out the flowchart and create named files appropriately.
    Also introducing macros.

---
+++ src/hello.c
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+int add_generate_hello() {
+
+}
--- src/module_hooks.c
+++ src/module_hooks.c
@@ -6,31 +6,47 @@
 int closing = 0;
 static void intrpt_routine(void *irrelevant);
 
-static struct delayed_work task;
-static DECLARE_DELAYED_WORK(task, intrpt_routine);
+static struct delayed_work hello_timer_dw;
+static DECLARE_DELAYED_WORK(hello_timer_dw, hello_timer);
+
+static struct delayed_work cleanup_module_dw;
+static DECLARE_DELAYED_WORK(cleanup_module_dw, add_cleanup);
+
+#define HELLO_INTERVAL (3*HZ)
+#define CLEANUP_INTERVAL (HELLO_INTERVAL + 1*HZ)
 
 /*******************************************************************************/
 int init_module(void) {
-    printk(KERN_INFO "adding add module 1");
-    printk(KERN_INFO "adding add module 2");
-    schedule_delayed_work(&task, 5*HZ);
+    printk(KERN_INFO "adding add kernel module");
+
+    add_init();
     return 0;
 }
 
-static void intrpt_routine(void *irrelevant) {
-    printk(KERN_INFO "INTERRUPT!!!");
+static void hello_timer(void *irrelevant) {
+    printk(KERN_INFO "hello_timer fired!");
+
+    add_generate_hello();
 
     if (closing == 0) {
-		schedule_delayed_work(&task, 5*HZ);
+		schedule_delayed_work(&hello_timer_dw, HELLO_INTERVAL);
     }
 }
 
 
 void cleanup_module(void) {
-    printk(KERN_INFO "removing add module 1");
-    printk(KERN_INFO "removing add module 2");
+    printk(KERN_INFO "removing add module");
     closing = 1;
-    cancel_delayed_work(&task);
+    cancel_delayed_work(&hello_timer_dw);
+    // schedule_delayed_work(&cleanup_module_dw, CLEANUP_INTERVAL);
 }
+
+int add_init() {
+    schedule_delayed_work(&hello_timer_dw, HELLO_INTERVAL);
+}
+
+// int add_cleanup() {
+
+// }
 
 MODULE_LICENSE("GPL");
